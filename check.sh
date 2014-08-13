fea7b9e5f3f1c5e5e2900228e1f6efa19341110d
Francois BERENGER <francois.berenger@inria.fr>
2014-08-13 15:17:59 +0200

    added check.sh to launch a bunch of unit tests

---
+++ check.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "1" | ./test
+echo "(1)" | ./test
+echo "toto" | ./test
+echo "True" | ./test
+echo "False" | ./test
+
+echo "1 * 2" | ./test
+echo "1 / 2" | ./test
+
+echo "1 + 1" | ./test
+echo "1 - 1" | ./test
+
+echo "True && False" | ./test
+echo "True || False" | ./test
+echo "fun x -> x 1" | ./test
+echo "fun x -> x" | ./test
+echo "let x = 1 in x" | ./test
