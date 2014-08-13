91b3c00869bf098da4916a13352e8f1c6093192b
Francois BERENGER <francois.berenger@inria.fr>
2014-08-13 11:56:05 +0200

    lex then parse until EOF

--- main.ml
+++ main.ml
@@ -3,9 +3,7 @@ open Ast
 let _ =
   try
     let lexbuf = Lexing.from_channel stdin in
-    while true do
-      let result = Parser.main Lexer.token lexbuf in
-      Printf.printf "%s\n%!" (string_of_expr result)
-    done
+    let result = Parser.main Lexer.token lexbuf in
+    Printf.printf "%s\n%!" (string_of_expr result)
   with Lexer.Eof ->
     exit 0
