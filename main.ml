94e75510ee120c8c32583aa3e952feeda29aec54
Francois BERENGER <francois.berenger@inria.fr>
2014-08-13 11:15:39 +0200

    force flush of stdout

--- main.ml
+++ main.ml
@@ -5,7 +5,7 @@ let _ =
     let lexbuf = Lexing.from_channel stdin in
     while true do
       let result = Parser.main Lexer.token lexbuf in
-      Printf.printf "%s\n" (string_of_const result)
+      Printf.printf "%s\n%!" (string_of_const result)
     done
   with Lexer.Eof ->
     exit 0
