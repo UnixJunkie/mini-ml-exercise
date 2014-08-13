e9d58b042a6d7aa66266f3b7fc9e8662ce9cdd9d
Francois BERENGER <francois.berenger@inria.fr>
2014-08-13 16:56:41 +0200

    trying to do the De Bruijn indexes computation

--- ast.ml
+++ ast.ml
@@ -7,6 +7,9 @@ type const = True | False | Int of int
 
 type var = string
 
+(* var with its De Bruijn index *)
+type db_var = string * int
+
 type bin_op = Plus | Minus | Mult | Div | And | Or
 
 (* expressions *)
@@ -19,6 +22,15 @@ type expr =
   | Fun of (var * expr) (* fonction definition *)
   | Let of (var * expr * expr) (* bind a variable into an expression *)
 
+(* expr with db_var instead of var *)
+type db_expr =
+  | Const of const
+  | Var of db_var
+  | Bin_op of expr * bin_op * expr
+  | Apply of expr * expr
+  | Fun of (db_var * expr)
+  | Let of (db_var * expr * expr)
+
 (* printers *)
 
 let string_of_const = function
@@ -26,8 +38,10 @@ let string_of_const = function
   | False -> "False"
   | Int i -> string_of_int i
 
-let string_of_var = function
-  | name -> name
+(* let string_of_var (name, index) = *)
+(*   Printf.sprintf "%s_%d" name index *)
+let string_of_var name =
+  name
 
 let string_of_bin_op = function
   | Plus  -> "+"
@@ -37,7 +51,7 @@ let string_of_bin_op = function
   | And   -> "&&"
   | Or    -> "||"
 
-let rec string_of_expr = function
+let rec string_of_expr (e: expr) = match e with
   | Const c -> string_of_const c
   | Var v -> string_of_var v
   | Bin_op (e1, op, e2) ->
@@ -49,3 +63,11 @@ let rec string_of_expr = function
   | Let (v, init, in_expr) ->
     "let " ^ (string_of_var v) ^ " = " ^ (string_of_expr init) ^ " in " ^
     (string_of_expr in_expr)
+
+let rec compute_db_indexes (e: expr): db_expr = match e with
+  | Const cst -> failwith "not implemented yet"
+  | Var v -> failwith "not implemented yet"
+  | Bin_op (e1, op, e2) -> failwith "not implemented yet"
+  | Apply (e1, e2) -> failwith "not implemented yet"
+  | Fun (v, e) -> failwith "not implemented yet"
+  | Let (v, e1, e2) -> failwith "not implemented yet"
