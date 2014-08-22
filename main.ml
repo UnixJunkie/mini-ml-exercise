36c9291f0dba039077005987f72002034477a974
Francois BERENGER <francois.berenger@inria.fr>
2014-08-22 15:15:48 +0200

    can print De Bruijn expressions (expressions with De Bruijn indexed variables)

--- main.ml
+++ main.ml
@@ -3,7 +3,9 @@ open Ast
 let _ =
   try
     let lexbuf = Lexing.from_channel stdin in
-    let result = Parser.main Lexer.token lexbuf in
-    Printf.printf "%s\n%!" (string_of_expr result)
+    let e = Parser.main Lexer.token lexbuf in
+    Printf.printf "input: %s\n" (string_of_expr e);
+    let db_e = dbi_indexes [] e in
+    Printf.printf "output: %s\n" (string_of_db_expr db_e)
   with Lexer.Eof ->
     exit 0
