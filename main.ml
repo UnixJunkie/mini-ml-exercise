1b7dbf49f70ac468ae53661edd9c4df30d96c96a
Francois BERENGER <francois.berenger@inria.fr>
2014-08-25 15:06:47 +0200

    call the interpreter at the end

--- main.ml
+++ main.ml
@@ -6,6 +6,8 @@ let _ =
     let e = Parser.main Lexer.token lexbuf in
     Printf.printf "input: %s\n" (string_of_expr e);
     let db_e = dbi_indexes [] e in
-    Printf.printf "output: %s\n" (string_of_db_expr db_e)
+    Printf.printf "output: %s\n" (string_of_db_expr db_e);
+    let res = interpret db_e in
+    Printf.printf "res: %s\n" (string_of_value res);
   with Lexer.Eof ->
     exit 0
