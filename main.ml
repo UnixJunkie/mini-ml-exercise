47d484d4212669c5556373a64d04f4cc7e36c490
Francois BERENGER <francois.berenger@inria.fr>
2014-09-04 14:18:21 +0200

    - try to catch exceptions during interpretation - can compile - can execute compilation's output

--- main.ml
+++ main.ml
@@ -1,13 +1,22 @@
 open Ast
+open Printf
 
 let _ =
   try
     let lexbuf = Lexing.from_channel stdin in
     let e = Parser.main Lexer.token lexbuf in
-    Printf.printf "input: %s\n" (string_of_expr e);
+    printf "input: %s\n" (string_of_expr e);
     let db_e = dbi_indexes [] e in
-    Printf.printf "output: %s\n" (string_of_db_expr db_e);
-    let res = interpret db_e in
-    Printf.printf "res: %s\n" (string_of_value res);
+    printf "output: %s\n" (string_of_db_expr db_e);
+    (try
+       let ires = interpret db_e in
+       printf "res: %s\n" (string_of_value ires);
+     with Error msg ->
+       printf "interpretation failure: %s\n" msg
+    );
+    let compiled = compile db_e in
+    printf "compiled: %s\n" (string_of_instructions compiled);
+    let cres = run compiled in
+    printf "compiled: %s\n" (string_of_vm_state cres)
   with Lexer.Eof ->
     exit 0
